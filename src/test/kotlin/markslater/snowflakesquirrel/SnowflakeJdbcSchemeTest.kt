44464c6814069056ac69e439eb089c85da4b99c3
Mark Slater <mark.slater@mail.com>
2021-01-28 13:21:16 +0000

    Tidy.

--- src/test/kotlin/markslater/snowflakesquirrel/SnowflakeJdbcSchemeTest.kt
+++ src/test/kotlin/markslater/snowflakesquirrel/SnowflakeJdbcSchemeTest.kt
@@ -5,6 +5,8 @@ import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.has
 import com.natpryce.hamkrest.isA
 import net.sourceforge.urin.*
+import net.sourceforge.urin.Authority.authority
+import net.sourceforge.urin.Host.registeredName
 import net.sourceforge.urin.scheme.http.HttpQuery
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -12,9 +14,7 @@ import org.junit.jupiter.api.Test
 internal class SnowflakeJdbcSchemeTest {
     @Test
     fun `Can make sample Snowflake connection string`() {
-        val urin: Urin<Iterable<Iterable<String>>, HttpQuery, Fragment<String>> = SnowflakeJdbcScheme.urin(
-            Authority.authority(Host.registeredName("xy12345.snowflakecomputing.com"))
-        )
+        val urin = SnowflakeJdbcScheme.urin(authority(registeredName("xy12345.snowflakecomputing.com")))
         assertThat(urin, isA(has(Urin<Iterable<Iterable<String>>, HttpQuery, Fragment<String>>::asString,
             equalTo("jdbc:snowflake://xy12345.snowflakecomputing.com")))
         )
