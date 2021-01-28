1cd269c3a6c61cec900c7161e625d7760311b254
Mark Slater <mark.slater@mail.com>
2021-01-28 16:06:22 +0000

    Streaming stage of data.

--- src/main/kotlin/markslater/snowflakesquirrel/SnowflakeSquirrel.kt
+++ src/main/kotlin/markslater/snowflakesquirrel/SnowflakeSquirrel.kt
@@ -1,11 +1,14 @@
 package markslater.snowflakesquirrel
 
+import net.snowflake.client.jdbc.SnowflakeConnection
 import net.sourceforge.urin.Authority.authority
 import net.sourceforge.urin.Host.registeredName
 import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
-import java.sql.*
+import java.sql.Connection
+import java.sql.DriverManager
 import java.util.*
+
 
 fun main(args: Array<String>) {
     val propertiesFileName = args[0]
@@ -24,45 +27,11 @@ fun main(args: Array<String>) {
     val connection: Connection = getConnection(account, user, password)
     println("Done creating JDBC connection\n")
 
-    // create statement
-    println("Create JDBC statement")
-    val statement: Statement = connection.createStatement()
-    println("Done creating JDBC statement\n")
+    connection.unwrap(SnowflakeConnection::class.java).uploadStream(
+        "~", "testUploadStream",
+        """[{"foo":"bar"}, {"foo":"baz"}]""".byteInputStream(UTF_8), "sample.json", true
+    )
 
-    // create a table
-    println("Create demo table")
-    statement.executeUpdate("create or replace table demo(c1 string)")
-    println("Done creating demo table\n")
-
-    // insert a row
-    println("Insert 'hello world'")
-    statement.executeUpdate("insert into demo values ('hello world')")
-    println("Done inserting 'hello world'\n")
-
-    // query the data
-    println("Query demo")
-    val resultSet: ResultSet = statement.executeQuery("select * from demo")
-    println("Metadata:")
-    println("================================")
-
-    // fetch metadata
-    val resultSetMetaData = resultSet.metaData
-    println("Number of columns=" + resultSetMetaData.columnCount)
-    for (colIdx in 0 until resultSetMetaData.columnCount) {
-        println(
-            "Column " + colIdx + ": type=" + resultSetMetaData.getColumnTypeName(colIdx + 1)
-        )
-    }
-
-    // fetch data
-    println("\nData:")
-    println("================================")
-    val rowIdx = 0
-    while (resultSet.next()) {
-        println("row " + rowIdx + ", column 0: " + resultSet.getString(1))
-    }
-    resultSet.close()
-    statement.close()
     connection.close()
 }
 
